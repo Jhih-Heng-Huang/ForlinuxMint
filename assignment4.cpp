77e118848af99865b0661891fb9a92f0fa7c816e
Jhih-Heng Huang <kklive128@gmail.com>
2013-12-10 16:36:37 +0800

    finish the function of reading the input

--- assignment4.cpp
+++ assignment4.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <vector>
 #include <cstdlib>
 #include <iostream>
 
@@ -8,11 +10,35 @@ struct ineq
 	int constant;
 };
 
+void readInput( std::vector<ineq>& buffer, std::string inputFile )
+{
+	std::ifstream file;
+	ineq area;
+	int numArea;
+
+	file.open(inputFile.c_str());
+
+	file >> numArea;
+
+	for( int i = 0; i < numArea; ++i )
+	{
+		file >> area.coefX >> area.coefY >> area.constant;
+		buffer.push_back(area);
+	}
+
+	file.close();
+}
+
 int main()
 {
 	std::vector<ineq> buffer;
 
-	
+	readInput(buffer,"input.txt");
+
+	for( size_t i = 0; i < buffer.size(); ++i )
+	{
+		std::cout << buffer[i].coefX << "\t" << buffer[i].coefY << "\t" << buffer[i].constant << std::endl;
+	}
 	
 	return 0;
 }
